b42c1de4f3061bc4816ce56e9c4a66d17d3f1bfd
Yannick Fricke <yannickfricke@googlemail.com>
2022-02-19 04:43:10 +0100

    added the ExUnitHelpers module
    The module can be used in tests to automatically require
    the correct modules + defining some generator functions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,21 @@
 ExUnit.start()
+
+defmodule ExUnitHelpers do
+  @moduledoc false
+
+  defmacro __using__(_opts) do
+    quote do
+      use ExUnit.Case, async: true
+      use ExUnitProperties
+    end
+  end
+
+  def version_generator do
+    StreamData.map(
+      {StreamData.integer(0..9), StreamData.integer(0..9), StreamData.integer(0..9)},
+      fn {major, minor, patch} ->
+        StreamData.constant("#{major}.#{minor}.#{patch}")
+      end
+    )
+  end
+end
